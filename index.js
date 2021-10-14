7606b338e46ca3fd842051612cd4008007bb011f
JD <jdmejia712@gmail.com>
2021-10-13 23:54:40 -0400

    commit tree 9014870c0b4cf54d81636465a4b5f0ecd934858e

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+
+function superbowlWin(inventory){
+ 
+    let winningYear = inventory.find(recordYear => recordYear.result === 'W');
+    return !!winningYear ? winningYear.year : undefined
+}
+ 
+// array1.filter(element => element > 10);
+// console.log(record.find(superbowlWin))
